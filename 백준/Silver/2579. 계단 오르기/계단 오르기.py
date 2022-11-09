44f2785dd8e21d01a1fc2f5713ef2d3221255f94
SeongJaeBae <113089704+SeongJaeBae@users.noreply.github.com>
2022-11-09 14:03:48 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,23 @@
+
+def main():
+    
+    #답지봄
+    N = int(input())
+    stair = [0]*301
+    for i in range(N):
+        stair[i]=int(input())
+
+    DP = [0]*301
+    DP[0] = stair[0]
+    DP[1] = stair[0]+stair[1]
+    DP[2] = max(stair[0]+stair[2], stair[1]+stair[2])
+
+    for i in range(3, N):
+        DP[i] = max(DP[i-3] + stair[i-1] + stair[i], DP[i-2]+stair[i])
+
+    print(DP[N-1])
+
+
+
+if __name__ == "__main__":
+    main()
