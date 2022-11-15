94aeb51a73cf49a4ff76550fe69a6c8438af34b6
배성재 <113089704+SeongJaeBae@users.noreply.github.com>
2022-11-15 22:08:36 +0900

    [Bronze III] Title: 최소, 최대, Time: 404 ms, Memory: 149432 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,15 @@
+
+def main():
+    n = int(input())
+    s =  list(map(int,input().split()))
+    
+    min_val = min(s)
+    max_val = max(s)
+    print(min_val, max_val)
+
+    
+    
+        
+if __name__ == "__main__":
+    main()
+
