1c3474262982483b29170047cab6ea7d938d2ce9
Seong Jae Bae <113089704+SeongJaeBae@users.noreply.github.com>
2022-11-30 22:08:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i,j in zip(absolutes, signs):
+        if j :
+            answer +=i
+        else :
+            answer -= i
+            
+    return answer
