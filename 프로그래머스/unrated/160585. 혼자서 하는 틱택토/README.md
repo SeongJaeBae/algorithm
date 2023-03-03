d5ebe03dc13960fa7eb747484f48065b9e372dda
Seong Jae Bae <113089704+SeongJaeBae@users.noreply.github.com>
2023-03-03 10:16:15 +0900

    [unrated] Title: 혼자서 하는 틱택토, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/README.md
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/README.md
@@ -1,10 +1,10 @@
 # [unrated] 혼자서 하는 틱택토 - 160585 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160585) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.3 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/unrated/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,31 +1,13 @@
-# Check if there is a winning row, column, or diagonal
-def check_win(player, board):
-    # Check rows
-    for i in range(3):
-        if all(cell == player for cell in board[i]):
-            return True
-
-    # Check columns
-    for j in range(3):
-        if all(board[i][j] == player for i in range(3)):
-            return True
-
-    # Check diagonals
-    if all(board[i][i] == player for i in range(3)):
-        return True
-    if all(board[i][2-i] == player for i in range(3)):
-        return True
-
-    return False
-
 def solution(board):
-    num_x = sum(row.count('X') for row in board)
-    num_o = sum(row.count('O') for row in board)
-
-    if num_x - num_o > 0 or abs(num_x - num_o) > 1:
-        return 0
-
-    elif (check_win('O', board) and num_x != num_o - 1) or (check_win('X', board) and num_x != num_o):
-        return 0
-
+    cnt1 = sum([a.count("O") for a in board])
+    cnt2 = sum([a.count("X") for a in board])
+    tmp = [[(0, 0), (0, 1), (0, 2)], [(1, 0), (1, 1), (1, 2)], [(2, 0), (2, 1), (2, 2)], 
+           [(0, 0), (1, 0), (2, 0)], [(0, 1), (1, 1), (2, 1)], [(0, 2), (1, 2), (2, 2)],
+           [(0, 0), (1, 1), (2, 2)], [(2, 0), (1, 1), (0, 2)]]
+    a = sum([all([board[x][y] == "O" for x, y in arr]) for arr in tmp])
+    b = sum([all([board[x][y] == "X" for x, y in arr]) for arr in tmp])
+    if not (0 <= cnt1 - cnt2 <= 1): return 0
+    if a + b > 1 and not (a == 2 and b == 0 and cnt1 - cnt2 == 1): return 0
+    if a == 1 and cnt1 == cnt2: return 0
+    if b == 1 and cnt1 > cnt2: return 0
     return 1
