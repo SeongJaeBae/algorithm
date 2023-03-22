20fc1f3d7c3da9f26129f0e98ce23db0b5e2f609
Seong Jae Bae <113089704+SeongJaeBae@users.noreply.github.com>
2023-03-23 00:56:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 6020 ms, Memory: 282468 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,21 +2,19 @@ import sys
 
 N, K = map(int, input().split())
 stuff = [[0,0]]
-knapsack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+sack = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
 
 for _ in range(N):
     stuff.append(list(map(int, input().split())))
 
-
-#냅색 문제 풀이
 for i in range(1, N + 1):
     for j in range(1, K + 1):
-        weight = stuff[i][0] 
-        value = stuff[i][1]
+        w= stuff[i][0] 
+        v= stuff[i][1]
        
-        if j < weight:
-            knapsack[i][j] = knapsack[i - 1][j] #weight보다 작으면 위의 값을 그대로 가져온다
+        if j < w:
+            sack[i][j] = sack[i - 1][j]
         else:
-            knapsack[i][j] = max(value + knapsack[i - 1][j - weight], knapsack[i - 1][j])
+            sack[i][j] = max(v+ sack[i - 1][j - w], sack[i - 1][j])
 
-print(knapsack[N][K])
+print(sack[N][K])
