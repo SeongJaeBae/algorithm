643f64fc50848026a6311694c86480629b9e8934
Seong Jae Bae <113089704+SeongJaeBae@users.noreply.github.com>
2023-03-27 09:32:18 +0900

    [level 3] Title: 불량 사용자, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,27 @@
+from itertools import product
+
+def check(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    for i in range(len(str1)):
+        if str1[i] == "*":
+            continue
+        if str1[i] != str2[i]:
+            return False
+    return True
+
+def solution(user_id, banned_id):
+    answer = set()
+    result = [[] for i in range(len(banned_id))]
+
+    for i in range(len(banned_id)):
+        for u in user_id:
+            if check(banned_id[i], u):
+                result[i].append(u)
+
+    result = list(product(*result))
+    for r in result:
+        if len(set(r)) == len(banned_id):
+            answer.add("".join(sorted(set(r))))
+
+    return len(answer)
